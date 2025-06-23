a85182b51d82a379e0cc068d8283ecead16b17fc
Arenitast <luiss.arenast@gmail.com>
2025-06-23 15:22:29 -0500

    Refactoriza pruebas de UI: añade validaciones y tags de prueba

--- app/src/androidTest/java/com/example/proyecto_movil_parcial/FirstPresentationScreenTest.kt
+++ app/src/androidTest/java/com/example/proyecto_movil_parcial/FirstPresentationScreenTest.kt
@@ -1,4 +1,65 @@
 package com.example.proyecto_movil_parcial
 
-class FirstPresentationScreenTest {
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.performClick
+import org.junit.Rule
+import org.junit.Test
+
+class PresentationScreenTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun buttonDisabled_whenInputEmpty() {
+        composeTestRule.setContent {
+            PresentationScreen(onFinish = {})
+        }
+        composeTestRule.onNodeWithTag("input_npalabras").assertIsDisplayed()
+        composeTestRule.onNodeWithTag("btn_siguiente").assertIsNotEnabled()
+    }
+
+    @Test
+    fun buttonEnabled_whenValidInput() {
+        composeTestRule.setContent {
+            PresentationScreen(onFinish = {})
+        }
+        composeTestRule.onNodeWithTag("input_npalabras").performTextInput("10")
+        composeTestRule.onNodeWithTag("btn_siguiente").assertIsEnabled()
+    }
+
+    @Test
+    fun buttonDisabled_whenInputTooHigh() {
+        composeTestRule.setContent {
+            PresentationScreen(onFinish = {})
+        }
+        composeTestRule.onNodeWithTag("input_npalabras").performTextInput("51")
+        composeTestRule.onNodeWithTag("btn_siguiente").assertIsNotEnabled()
+    }
+
+    @Test
+    fun buttonDisabled_whenInputIsZero() {
+        composeTestRule.setContent {
+            PresentationScreen(onFinish = {})
+        }
+        composeTestRule.onNodeWithTag("input_npalabras").performTextInput("0")
+        composeTestRule.onNodeWithTag("btn_siguiente").assertIsNotEnabled()
+    }
+
+    @Test
+    fun onFinishCalled_whenValidInputAndClick() {
+        var callbackInvokedWith: Int? = null
+        composeTestRule.setContent {
+            PresentationScreen(onFinish = { callbackInvokedWith = it })
+        }
+        composeTestRule.onNodeWithTag("input_npalabras").performTextInput("20")
+        composeTestRule.onNodeWithTag("btn_siguiente").performClick()
+
+        assert(callbackInvokedWith == 20)
+    }
 }
